c67af8499a6d55c006fd2772809b296d7463bb83
shokanastasy <anastasy.shokurova@gmail.com>
2022-05-16 14:12:26 +0300

    lesson3

---
+++ src/main/java/lesson3/SeleniumStart.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumStart {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://google.com");
+
+        Thread.sleep(5000);
+        chromeDriver.quit();
+    }
+}
